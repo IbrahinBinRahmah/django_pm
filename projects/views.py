dbb267f642a23f92492e340dd881a214c11c08e6
IbrahinBinRahmah <ibrahimbinrahma@gmail.com>
2025-03-02 02:24:48 +0300

    Login & Logout

--- projects/views.py
+++ projects/views.py
@@ -4,8 +4,9 @@ from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from . import models
 from . import forms
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class ProjectListView(ListView):
+class ProjectListView(LoginRequiredMixin, ListView):
     model = models.Project
     template_name = 'project/list.html'
     paginate_by = 6
@@ -18,41 +19,41 @@ class ProjectListView(ListView):
             where['title__icontains'] = q
         return query_set.filter(**where)
 
-class ProjectCreateView(CreateView):
+class ProjectCreateView(LoginRequiredMixin, CreateView):
     model = models.Project
     form_class = forms.ProjectCreateForm
     template_name = 'project/create.html'
     success_url = reverse_lazy('Project_list')
 
-class ProjectUpdateView(UpdateView):
+class ProjectUpdateView(LoginRequiredMixin, UpdateView):
     model = models.Project
     form_class = forms.ProjectUpdateForm
     template_name = 'project/update.html'
     def get_success_url(self):
         return reverse('Project_update', args=[self.object.id])
 
-class ProjectDeleteView(DeleteView):
+class ProjectDeleteView(LoginRequiredMixin, DeleteView):
     model = models.Project
     template_name = 'project/delete.html'
 
     success_url = reverse_lazy('Project_list')
 
 
-class TaskCreateView(CreateView):
+class TaskCreateView(LoginRequiredMixin, CreateView):
     model = models.Task
     fields = ['project', 'description']
-    http_method_names = ['post']
+    http_method_names = ['get', 'post']
     def get_success_url(self):
         return reverse('Project_update', args=[self.object.project.id])
 
-class TaskUpdateView(UpdateView):
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
     model = models.Task
     fields = ['is_completed']
-    http_method_names = ['post']
+    http_method_names = ['get', 'post']
     def get_success_url(self):
         return reverse('Project_update', args=[self.object.project.id])
 
-class TaskDeleteView(DeleteView):
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
     model = models.Task
 
     def get_success_url(self):
--- projects_management/settings.py
+++ projects_management/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'projects_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,6 +131,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login'
 
 INTERNAL_IPS = [
     "127.0.0.1",
