734a1101f28d5cb76316906374d8aac22112ddaf
IbrahinBinRahmah <ibrahimbinrahma@gmail.com>
2025-03-03 03:28:31 +0300

    Admin Panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from projects.models import Category, Project, Task
+from . import models
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Project)
+admin.site.register(Task)
